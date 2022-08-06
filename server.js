68a7010af61765621b7f38955c96a4569872ba3f
Patrick Poopathi <patrickgp1992@hotmail.com>
2022-08-06 09:00:02 -0400

    establish PORT and test GET call

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Test GET
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello World",
+  });
+});
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+// Starts Express.js server on port designated above
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
